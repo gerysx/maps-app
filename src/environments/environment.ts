1df5d35cdb96d7c98b06e05e8859512984f3c006
Germán Álvarez <156426040+gerysx@users.noreply.github.com>
2025-08-05 10:48:36 +0200

    envs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,3 +2,5 @@
 export const environment = {
   mapboxkey: "pk.eyJ1IjoiZ2VyeXNkZXYiLCJhIjoiY21kcmxrdDh0MGZqMjJrc2JtZ3lsZ3R3cCJ9.T9_2SsRQTYmbtKk5zhRP2w"
 };
+
+//ESTA API_KEY ESTÁ RESTRINGIDA, AUNQUE LA COPIES NO PODRÁ SER UTILIZADA
